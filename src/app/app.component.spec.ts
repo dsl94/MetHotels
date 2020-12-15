1ab5966ba3d2c0fd9ba9a4fff810192d0f1afe9e
Nemanja Petrovic <nemanja.petrovic@badin.rs>
2020-12-15 14:53:07 +0100

    IT255_DZ14

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,9 +1,16 @@
-import { TestBed } from '@angular/core/testing';
+import { fakeAsync, TestBed, tick } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { ConsoleSpy } from './utils';
+
+var fakeConsole;
 
 describe('AppComponent', () => {
   beforeEach(async () => {
+    fakeConsole = new ConsoleSpy();
+    let originalConsole = window.console;
+    (<any>window).console = fakeConsole;
     await TestBed.configureTestingModule({
       imports: [
         RouterTestingModule
@@ -32,4 +39,17 @@ describe('AppComponent', () => {
     const compiled = fixture.nativeElement;
     expect(compiled.querySelector('.content span').textContent).toContain('met-hotels app is running!');
   });
+
+  
+
+  it('handles form changes', fakeAsync(() => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    let input = fixture.debugElement.query(By.css('input')).nativeElement;
+    input.value = 'XYZ';
+    dispatchEvent(input);
+    tick();
+ 
+    expect(fakeConsole.logs).toContain('form changed to: [object Object]');
+  }));
 });
